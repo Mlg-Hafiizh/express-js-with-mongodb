b97e641301c534a3e9e7efe75fa49227d9db5606
Hafiizh Asrofil A <71694463+Mlg-Hafiizh@users.noreply.github.com>
2022-06-15 15:54:08 +0700

    update 15 jun 2022

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const express   = require("express");
-const app       = express();
+const express       = require("express");
+const dotenv        = require("dotenv");
+const morgan        = require("morgan");
+const bodyparser    = require("body-parser");
+const path          = require("path");
 
-app.get('/', (req,res) => {
-    res.send("express-mongoDB");
-});
+const connectDB = require('./server/databases/connection');
 
-app.listen(3000, ()=> {console.log('Server is running on http://localhost:${3000}')});
+const app           = express();
+
+dotenv.config({path:'config.env'});
+const PORT = process.env.PORT || 8080;
+
+// log requests
+app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+// menambahkan folder ke dalam engine ejs
+app.set("view engine","ejs");
+
+// menambahkan sub folder ke dalam engine ejs
+//app.set("views",path.resolve(__dirname,"views/ejs"));
+
+// load assets
+app.use("/css",express.static(path.resolve(__dirname,"assets/css"))); 
+app.use("/images",express.static(path.resolve(__dirname,"assets/images"))); 
+app.use("/js",express.static(path.resolve(__dirname,"assets/js"))); 
+
+// load routers
+app.use('/', require('./server/routes/router'))
+
+app.listen(PORT, ()=> {console.log(`Server is running on http://localhost:${PORT}`)});
